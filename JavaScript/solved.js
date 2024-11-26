b30aa8c31561b0a48381729f659f96ef5667c510
mazumdaratik <rjatik990@gmail.com>
2024-11-26 21:53:56 +0600

    last solved

--- JavaScript/solved.js
+++ JavaScript/solved.js
@@ -8,7 +8,7 @@ function table (num1) {
 table(5);
 table(10);
 //2.Program to make a simple calculator using switch case in JavaScript
-let num2 = prompt("Enter 1st digit:");
+/* let num2 = prompt("Enter 1st digit:");
 let num22 = prompt("Enter 2nd digit:");
 let operator = prompt ("Please, Select the operator + - x /");
 
@@ -32,5 +32,46 @@ let operator = prompt ("Please, Select the operator + - x /");
         break;
         default:
         console.log("Something Wrong!")
-    }
+    } */
     
+//3.Program to Check if the Numbers Have Same Last Digit
+let num3 = 42;
+let num33 = 52;
+let num333 = 62;
+
+let result1 = num3 % 10 ;
+let result2 = num33 % 10 ;
+let result3 = num333 % 10 ;
+
+if (result1 == result2 && result1 == result3) {
+    console.log(`The last digit of ${num3} , ${num33} , ${num333} are Same.`);
+} else {
+    console.log(`The last digit of ${num3} , ${num33} , ${num333} are Not Same.`);
+}
+
+//4.Program to Print the Fibonacci series.
+
+let a = 0, 
+    b = 1 ;
+    console.log(a);
+    console.log(b);
+    for ( let i = 0; i<=10; i++) {
+        let temp = a + b; //0+1 = 1
+        console.log(temp); //1
+        a = b; //0=1 => 1
+        b = temp;  // 1 = 1 => 1
+    }
+
+//4.1 
+let fibonacci = (f) =>{
+    let series = [];
+    let x = 0 , y = 1;
+    for (let i = 0; i <= 7; i++) {
+        series.push(x); // [0,]
+        let tem = x + y;
+        x = y;
+        y = tem;
+    }
+    return series;
+};
+console.log(fibonacci(10))
