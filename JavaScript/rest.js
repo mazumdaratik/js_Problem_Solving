52b873dbb878a2d43ca4aa092b5c07622b961235
mazumdaratik <rjatik990@gmail.com>
2024-12-27 20:14:17 +0600

    Spread & Rest added

---
+++ JavaScript/rest.js
@@ -0,0 +1,4 @@
+function numbers(a, ...params){
+    console.log(params);
+}
+numbers(9, 8, 7, 6, 5, 4);
---
+++ JavaScript/spread.js
@@ -0,0 +1,5 @@
+let numbers = [ 1, 2, 3];
+
+// let newNumbers = [numbers[0], numbers[1],numbers[2], 4, 5, 6];
+let newNumbers = [...numbers, 4, 5,6];
+console.log(newNumbers);
