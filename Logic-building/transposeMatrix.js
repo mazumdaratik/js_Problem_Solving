5068dd21a2b824ef8a115847365d3ceb503eb210
mazumdaratik <rjatik990@gmail.com>
2025-01-17 19:13:32 +0600

    Transpose Matrix added

---
+++ Logic-building/transposeMatrix.js
@@ -0,0 +1,26 @@
+function transposeMatrix(inputArray){
+    
+    const numOfRows = inputArray.length;
+    const numOfCols = inputArray[0].length;
+
+    const result = [];
+
+    for(let i = 0; i < numOfRows; i++){
+        for(let j = 0; j < numOfCols; j++){
+            console.log(inputArray [i][j]);
+            if(!result[j]){
+                result [j] = [];
+            }
+
+            result [j] [i] = inputArray [i] [j]
+        }
+        
+    }
+
+    return result;
+}
+
+const inputMatrix = [ [3, 4, 8],
+                      [5, 6, 9] ];
+
+console.log(transposeMatrix(inputMatrix));
