4a87b72af0a76e32d4966b17b125aa1d8dc1c263
mazumdaratik <rjatik990@gmail.com>
2025-03-07 22:47:36 +0600

    symbol added

--- JavaScript/symbols.js
+++ JavaScript/symbols.js
@@ -1,8 +1,15 @@
-var cricket = Symbol('this is crickeet');
+/* var cricket = Symbol('this is crickeet');
 
 var object = {
     name: 'Tamim',
     [cricket]: 3
 };
 
-console.log(object);
+console.log(object); */
+var object = {};
+object.name = 'JavaScript';
+object.estd = '1995';
+
+object[Symbol('founder')] = "Brenden Eich";
+
+console.log(Object.keys(object));
