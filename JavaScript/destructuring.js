3e97552391ce15ce031e03573f798aec586cfda7
mazumdaratik <rjatik990@gmail.com>
2025-03-16 02:04:03 +0600

    array destructuring added

--- JavaScript/destructuring.js
+++ JavaScript/destructuring.js
@@ -1,3 +1,4 @@
+//object
 const player = {
     jersyNo: 45,
     name: "Tamim",
@@ -10,3 +11,14 @@ const player = {
 
 const {Type:{Batting, Bowling}, } = player;
 console.log(Batting, Bowling);
+
+//array
+var odds = [1, 3, 5, [7, 9, 11], 13];
+var [, , , [, nine, eleven], ,] = odds;
+console.log(nine, eleven);
+
+var a = 1;
+var b = 2;
+
+[b, a] = [a, b];
+console.log(a, b);
