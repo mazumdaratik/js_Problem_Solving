96aad076ff463d57828043921f7c9fb875a6c31e
mazumdaratik <rjatik990@gmail.com>
2025-03-18 23:41:20 +0600

    store fibonacci nums in a Array

--- Logic-building/fibonacci.js
+++ Logic-building/fibonacci.js
@@ -12,3 +12,16 @@ function fibonacci(n) {
 
 console.log(fibonacci(6));
 console.log(fibonacci(10)); 
+//store fibonacci numbers in an Array
+function fibonacciArray(n) {
+    let arr = [0, 1]; // First two numbers
+
+    for (let i = 2; i < n; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
+
+    return arr;
+}
+
+console.log(fibonacciArray(10));
+
