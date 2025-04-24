8e314ab20c83b80b7fce75006d9439cb862c77da
mazumdaratik <rjatik990@gmail.com>
2025-04-24 22:32:14 +0600

    typescript added

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -2,3 +2,52 @@ function addNumbers(num1, num2) {
     console.log(num1 + num2);
 }
 addNumbers(20, 30);
+var userId;
+var firstName;
+var lastName;
+var isActivated;
+userId = 20303037;
+firstName = "atik";
+lastName = "mazumdar";
+isActivated = true;
+var fullName = firstName.concat(lastName);
+console.log(firstName + " " + lastName);
+console.log(fullName);
+function display(age) {
+    console.log(age);
+}
+display(24);
+var studentsName;
+studentsName = ['sami', 'rahin', 'mostafiz', 24, 30];
+console.log(studentsName.sort());
+var Type1;
+(function (Type1) {
+    Type1[Type1["readData"] = 0] = "readData";
+    Type1[Type1["saveData"] = 1] = "saveData";
+    Type1[Type1["deleteData"] = 2] = "deleteData";
+})(Type1 || (Type1 = {}));
+console.log(Type1);
+var Type2;
+(function (Type2) {
+    Type2["readData"] = "data is reading";
+    Type2["saveData"] = "saving data";
+    Type2[Type2["id"] = 101] = "id";
+})(Type2 || (Type2 = {}));
+console.log(Type2.readData);
+console.log(Type2.id);
+var userName;
+userName = "atik";
+userName = true;
+userName = 123456;
+var users;
+users = [];
+var user1;
+user1 = { userName: "Atik", userId: 101 };
+users.push(user1);
+var user2;
+user2 = { userName: "samiul", userId: 102 };
+users.push(user2);
+console.log(users);
+for (var key in users) {
+    console.log(users[key]);
+}
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -2,3 +2,60 @@ function addNumbers(num1: number, num2: number){
     console.log(num1 + num2);
 }
 addNumbers(20, 30);
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let isActivated: boolean;
+
+userId = 20303037
+firstName = "atik"
+lastName = "mazumdar"
+isActivated = true;
+let fullName: string = firstName.concat(lastName)
+console.log(firstName + " " + lastName);
+console.log(fullName);
+function display(age: number): void {
+    console.log(age)
+}
+display(24);
+
+let studentsName: (string | number) [];
+studentsName = ['sami', 'rahin', 'mostafiz', 24, 30];
+console.log(studentsName.sort());
+
+enum Type1{
+    readData,
+    saveData,
+    deleteData,
+}
+console.log(Type1);
+
+enum Type2{
+    readData = "data is reading",
+    saveData = "saving data",
+    id = 101,
+}
+console.log(Type2.readData);
+console.log(Type2.id);
+
+let userName: any;
+userName = "atik"
+userName = true
+userName = 123456
+
+let users: object[];
+users = [];
+
+let user1: {userName: string, userId: number};
+user1 = {userName: "Atik", userId: 101};
+users.push(user1);
+
+let user2: {userName: string, userId: number};
+user2 = {userName: "samiul", userId: 102};
+users.push(user2);
+console.log(users);
+
+for (const key in users) {
+    console.log(users[key]);
+}
